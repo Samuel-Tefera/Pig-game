9b18b4611112f6dbe5f2ebf967f315791a69378d
Samuel-Tefera <samuelteferab@gmail.com>
2024-07-17 15:02:20 +0300

    make a player to hold his current score and add it on total score

--- script.js
+++ script.js
@@ -3,19 +3,29 @@
 const score0El = document.getElementById( 'score--0' );
 const score1El = document.getElementById( 'score--1' );
 const diceEl = document.querySelector( '.dice' );
-const btnRoll = document.querySelector( '.btn--roll' )
-const current0El = document.getElementById( 'current--0' )
-const current1El = document.getElementById( 'current--1' )
-const player0El = document.querySelector('.player--0')
-const player1El = document.querySelector('.player--1')
+const btnRoll = document.querySelector( '.btn--roll' );
+const current0El = document.getElementById( 'current--0' );
+const current1El = document.getElementById( 'current--1' );
+const player0El = document.querySelector( '.player--0' );
+const player1El = document.querySelector( '.player--1' );
+const btnHold = document.querySelector( '.btn--hold' );
 
 let currentScore, activePlayer;
+
+const switchPlayer = function () {
+    document.getElementById( `current--${ activePlayer }` ).textContent = 0;
+    currentScore = 0;
+    activePlayer = ( activePlayer === 0 ) ? 1 : 0;
+    player0El.classList.toggle( 'player--active' );
+    player1El.classList.toggle( 'player--active' );
+};
 
 // Starting condition
 score0El.textContent = 0;
 score1El.textContent = 0;
 currentScore = 0;
 activePlayer = 0;
+const score = [ 0, 0 ];
 diceEl.classList.add( 'hidden' );
 
 //Rolling dice
@@ -32,13 +42,21 @@ btnRoll.addEventListener( 'click',
         if ( dice !== 1 ) {
             // Add dice to current score
             currentScore += dice;
-            document.getElementById( `current--${ activePlayer }` ).textContent = currentScore;
+            document.getElementById( `current--${ activePlayer }` ).textContent =
+                currentScore;
         }
         else {
-            document.getElementById( `current--${ activePlayer }` ).textContent = 0;
-            currentScore = 0;
-            activePlayer = ( activePlayer === 0 ) ? 1 : 0;
-            player0El.classList.toggle( 'player--active' );
-            player1El.classList.toggle( 'player--active' );
+            // Switch palyer
+            switchPlayer()
         }
+    } );
+
+btnHold.addEventListener( 'click',
+    function () {
+        // Add current score to active player's score
+        score[ activePlayer ] += currentScore;
+        document.getElementById( `score--${ activePlayer }` ).textContent =
+            score[ activePlayer ];
+        //switch player
+        switchPlayer();
     } );
